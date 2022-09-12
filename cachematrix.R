78ff8c408be4e82ca72082eb7e5d9d29ef775cc3
KalSidawi <kal.sidawi@gmail.com>
2022-09-12 01:00:41 -0600

    functions built and tested for matrix(NA)

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,42 @@
 ## functions do
 
 ## Write a short comment describing this function
+# this is the constructor function that creates a list of functions
+# set, get, setinverse, getinverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+    inv <- matrix(NA)
+    set <- function(y)
+    {
+      if (!is.matrix(y) | nrow(y) != ncol(y)) return(errorCondition("input is not a matrix or a non-square matrix"))
+      x <<- y
+      matrixna <- matrix(NA)
+      inv <<- matrixna
+    }
+    get <- function() x
+    setinverse <- function(inverse) 
+    {
+    # add validation condition on inverse
+      inv <<- inverse
+    }
+    getinverse <- function() inv
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!all(is.na(inv)))
+  {
+    message("retrieving inverse matrix from cache..")
+    return(inv)
+  }
+  inv <- solve(x$get())
+  x$setinverse(inv)
+  inv
 }
